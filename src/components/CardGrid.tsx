26cd6587250fee61de00eef4c2e6e5096fc082e4
Craig Summers <73234010+AlphaPentagon@users.noreply.github.com>
2022-09-21 16:00:48 +0100

    Trying different way of holding data

--- src/components/CardGrid.tsx
+++ src/components/CardGrid.tsx
@@ -15,6 +15,7 @@ const CardGrid = (): JSX.Element => {
   const [randomCards, setRandomCards] = useState<CardObj[] | null>(null);
   const [score, setScore] = useState<number>(0);
   const [selectedCards, setSelectedCards] = useState<CardObj[]>([]);
+  let numSelectedCards = 0;
 
   // randomly selects a card from the cardsArr, adds it to the randomOrderArr and then removes it from the original array, so that we end up with an array of cards in a random order
   const randomiseCards = () => {
@@ -31,20 +32,50 @@ const CardGrid = (): JSX.Element => {
     }
     return randomOrderArr;
   };
+  const randomCardsTest = randomiseCards();
   if (!randomCards) {
     const newState = randomiseCards();
     setRandomCards(newState);
+  } else if (selectedCards.length === 2) {
+    console.log(selectedCards);
+    if (selectedCards[0].name === selectedCards[1].name) {
+      setScore(score + 1);
+      const newState = randomCards.map((card) => {
+        if (card.name === selectedCards[0].name) {
+          return { ...card, found: true, flipped: false };
+        }
+        return card;
+      });
+      setSelectedCards([]);
+      setTimeout(() => setRandomCards(newState), 2000);
+    } else {
+      const newState = randomCards.map((card) => {
+        if (
+          card.name === selectedCards[0].name ||
+          card.name === selectedCards[1].name
+        ) {
+          return { ...card, flipped: false };
+        }
+        return card;
+      });
+      setSelectedCards([]);
+      setTimeout(() => setRandomCards(newState), 2000);
+    }
   }
 
   const handleClick = (cardIndex: number, card: CardObj) => {
-    setFlippedStatus(cardIndex);
-    updateSelectedCards(card);
-    checkSelectedCards();
+    if (numSelectedCards === 0 || numSelectedCards % 2 !== 0) {
+      setFlippedStatus(cardIndex);
+      updateSelectedCards(card);
+    }
   };
 
   const setFlippedStatus = (cardIndex: number) => {
     if (randomCards) {
-      if (randomCards[cardIndex].flipped === true) {
+      if (
+        randomCards[cardIndex].flipped === true ||
+        randomCards[cardIndex].found === true
+      ) {
         return;
       }
       console.log(randomCards[cardIndex]);
@@ -63,43 +94,19 @@ const CardGrid = (): JSX.Element => {
       console.log("Card already flipped.  Choose a different card");
       return;
     }
-    setSelectedCards([...selectedCards, card]);
-  };
-
-  const checkSelectedCards = () => {
-    if (randomCards) {
-      if (selectedCards.length === 2) {
-        if (selectedCards[0].name === selectedCards[1].name) {
-          setScore(score + 1);
-          const newState = randomCards.map((card) => {
-            if (card.name === selectedCards[0].name) {
-              return { ...card, found: true, flipped: false };
-            }
-            return card;
-          });
-          setRandomCards(newState);
-        } else {
-          const newState = randomCards.map((card) => {
-            if (
-              card.name === selectedCards[0].name ||
-              card.name === selectedCards[1].name
-            ) {
-              return { ...card, flipped: false };
-            }
-            return card;
-          });
-          setRandomCards(newState);
-        }
-        setSelectedCards([]);
-      }
+    if (card.found === true) {
+      console.log("Card already found.  Choose a different card");
+      return;
     }
+    setSelectedCards([...selectedCards, card]);
+    numSelectedCards++;
   };
 
   return (
     <Wrapper>
       <p>{score}</p>
-      {randomCards &&
-        randomCards.map((card, index) => (
+      {randomCardsTest &&
+        randomCardsTest.map((card, index) => (
           <Card
             handleClick={handleClick}
             card={card}
